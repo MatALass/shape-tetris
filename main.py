17f4b5743c4daa6f38436f248fd02ec28175739d
MatAlass <mathieu.alassoeur@efrei.net>
2026-03-27 16:15:44 +0100

    v1 create

--- main.py
+++
@@ -1,9 +0,0 @@
-# Projet Python : Comme un air de Tetris
-# Réalisé par Mathieu Alassoeur et Nathan Germany
-# Ce fichier est le main qui permet de lancer le jeu !
-
-
-import fonction_lancement_de_partie as f
-
-
-f.debut_du_jeu()
